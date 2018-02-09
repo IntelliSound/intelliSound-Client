dc15dc2f94862183ce5ea699afce24f33a12d9c2
David Lindahl <davidalindahl@gmail.com>
2018-02-08 18:22:11 -0800

    added multiline columns to the about us

--- src/component/about-us/index.js
+++ src/component/about-us/index.js
@@ -15,7 +15,7 @@ class About extends React.Component{
   render(){
     return (
       <div>
-        <div className="columns">
+        <div className="columns is-multiline is-centered">
 
           <div className="column">
             <div className="card">
